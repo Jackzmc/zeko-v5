c3f109e03c2f5e87e2e674a899126b145e7419ca
Jackz <jacksonbixby@gmail.com>
2023-06-25 21:51:13 -0500

    Own attempt

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,10 +1,22 @@
 use serenity::builder::CreateApplicationCommand;
+use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
 use serenity::model::prelude::interaction::application_command::CommandDataOption;
+use serenity::model::prelude::interaction::*;
+use serenity::prelude::*;
 
-pub fn run(_options: &[CommandDataOption]) -> String {
-    "Hey, I'm alive!".to_string()
+pub struct PingCommand;
+impl SlashCommand for PingCommand {
+    fn run(&mut self, ctx: &Context, interact: &ApplicationCommandInteraction, options: &[CommandDataOption]) -> String {
+        format!("Hey, {}", interact.user.name).to_string()
+    }
+
+    fn register<'a>(&'a self, command: &'a mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+        command.name("ping").description("A ping command")
+    }
 }
 
-pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-    command.name("ping").description("A ping command")
+pub trait SlashCommand {
+    fn run(&mut self, ctx: &Context, interact: &ApplicationCommandInteraction, options: &[CommandDataOption]) -> String;
+
+    fn register<'a>(&'a self, command: &'a mut CreateApplicationCommand) -> &mut CreateApplicationCommand;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::any::Any;
+use std::sync::Arc;
 use dotenv::dotenv;
 use serde_json::json;
 
@@ -7,6 +8,7 @@ use serenity::model::channel::Channel;
 use serenity::Client;
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use crate::commands::ping::{PingCommand, SlashCommand};
 
 mod util;
 mod commands;
@@ -22,20 +24,37 @@ impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         let guild_id = GuildId(std::env::var("GUILD_ID").expect("Expected GUILD_ID in environment").parse().expect("GUILD_ID must be an integer"));
         println!("[Discord] Connected as {}", ready.user.name);
-        let commands = guild_id.set_application_commands(&ctx.http, |commands| {
-            commands
-                .create_application_command(|command| commands::ping::register(command))
-        }).await;
+        // let commands = guild_id.set_application_commands(&ctx.http, |commands| {
+        //     commands
+        //         .create_application_command(|command| commands::ping::register(command))
+        // }).await;
     }
 
     async fn interaction_create(&self, ctx: Context, interaction: interaction::Interaction) {
-        if let Some(cmd) = interaction.application_command() {
-            cmd.create_interaction_response(&ctx.http, |r|
-                r.kind(model::application::interaction::InteractionResponseType::ChannelMessageWithSource)
-                    .interaction_response_data(|message| message.content("PONG"))
-            ).await.ok();
+        if let Some(interact_cmd) = interaction.application_command() {
+            let app_data = ctx.data.read().await;;
+            let data = app_data.get::<BotDataContainer>().unwrap();
+            let mut lock = data.lock().await;
+            for cmd in lock.commands.iter_mut() {
+                let content = cmd.run(&ctx, &interact_cmd, &interact_cmd.data.options);
+                // let content = commands::ping::run(&ctx, &interaction, &cmd.data.options);
+                interact_cmd.create_interaction_response(&ctx.http, |r|
+                    r.kind(model::application::interaction::InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                ).await.ok();
+            }
+
         }
     }
+}
+
+struct BotData {
+    commands: Vec<Box<dyn SlashCommand + Send>>
+}
+
+struct BotDataContainer;
+impl TypeMapKey for BotDataContainer {
+    type Value = Mutex<BotData>;
 }
 
 #[tokio::main]
@@ -43,13 +62,20 @@ async fn main() {
     dotenv::dotenv().ok();
     let token = std::env::var("DISCORD_TOKEN").expect("no discord token provided");
     let application_id = std::env::var("DISCORD_ID").expect("no discord id provided");
+    let guild_id = GuildId(std::env::var("GUILD_ID").expect("no GUILD_ID").parse::<u64>().expect("invalid guild id"));
+
+    let commands: Vec<Box<dyn SlashCommand + Send>> = vec![
+        Box::new(PingCommand)
+    ];
 
 
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
-
+    let bot_data = BotData {
+        commands
+    };
 
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
@@ -60,6 +86,17 @@ async fn main() {
     {
         let mut data = client.data.write().await;
         // data.insert::<ChatServerContainer>(manager);
+        data.insert::<BotDataContainer>(Mutex::new(bot_data));
+        for cmd in commands.iter() {
+            guild_id.create_application_command(&client.cache_and_http.http, |mut command| {
+                cmd.register(command)
+            }).await.expect("error registering");
+        }
+        // let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+        //     commands
+        //         .create_application_command(|command| commands::ping::register(command))
+        // })
+        //     .await;
     }
 
     if let Err(why) = client.start().await {
